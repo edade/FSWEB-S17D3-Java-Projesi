640ebd548b105e6508d04f9af74aad2f805dd98b
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-03-10 17:28:05 +0300

    controller

---
+++ src/main/java/com/workintech/s17d3/controller/KangarooController.java
@@ -0,0 +1,62 @@
+package com.workintech.s17d3.controller;
+
+import com.workintech.s17d3.entity.Kangaroo;
+import com.workintech.s17d3.validation.ZooKangarooValidation;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/kangaroo")
+public class KangarooController {
+    private Map<Integer, Kangaroo> kangaroos;
+
+    @PostConstruct void init(){
+        kangaroos = new HashMap<>();
+    }
+
+    @GetMapping
+    public List<Kangaroo> findAll(){
+        return kangaroos.values().stream().toList();
+    }
+
+    @GetMapping("/{id}")
+    public Kangaroo find(@PathVariable Integer id){
+        ZooKangarooValidation.isIdValid(id);
+        ZooKangarooValidation.checkKangarooExistence(kangaroos,id,true);
+        return kangaroos.get(id);
+
+    }
+
+    @PostMapping
+    public Kangaroo save(@RequestBody Kangaroo kangaroo){
+        ZooKangarooValidation.checkKangarooExistence(kangaroos,kangaroo.getId(),false);
+        ZooKangarooValidation.checkKangarooWeight(kangaroo.getWeight());
+        kangaroos.put(kangaroo.getId(),kangaroo);
+        return kangaroos.get(kangaroo.getId());
+    }
+
+    @PutMapping("/{id}")
+    public  Kangaroo update(@PathVariable("id") Integer id, @RequestBody Kangaroo kangaroo){
+        ZooKangarooValidation.isIdValid(id);
+        ZooKangarooValidation.checkKangarooWeight(kangaroo.getWeight());
+        kangaroo.setId(id);
+        if(kangaroos.containsKey(id)){
+            kangaroos.put(id,kangaroo);
+            return kangaroos.get(id);
+        }
+        else {
+            return save(kangaroo);
+        }
+
+    }
+    @DeleteMapping("/{id}")
+    public  Kangaroo delete(@PathVariable("id") Integer id){
+        ZooKangarooValidation.isIdValid(id);
+        ZooKangarooValidation.checkKangarooExistence(kangaroos,id,true);
+        return kangaroos.remove(id);
+    }
+}
